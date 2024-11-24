f11261c690b3925b4f47f1a494df1289ff6f8f37
Sadik <sadik@Sadiks-MBP.netis.cc>
2024-11-24 20:36:06 +0600

    second commit

--- app/page.js
+++ app/page.js
@@ -1,6 +1,13 @@
 "use client"
 import React from "react";
-import { useReactTable, createColumnHelper, getCoreRowModel,flexRender, getSortedRowModel} from "@tanstack/react-table";
+import {
+  useReactTable,
+  createColumnHelper,
+  getCoreRowModel,
+  flexRender,
+  getSortedRowModel,
+  getFilteredRowModel
+} from "@tanstack/react-table";
 import mockData from "@/data/data.json"
 import {
   ArrowUpDown,
@@ -18,72 +25,96 @@ const columnsHelper = createColumnHelper();
 
 const columns = [
   columnsHelper.accessor("id", {
-    cell:(info)=>info.getValue(), 
-    header: ()=>(
+    cell: (info) => info.getValue(),
+    header: () => (
       <span className="flex items-center">
-        <User size={16}/>Id
+        <User size={16} />Id
       </span>
     )
-  }), 
+  }),
   columnsHelper.accessor("name", {
-    cell: (info)=>info.getValue(), 
-    header:()=>(
+    cell: (info) => info.getValue(),
+    header: () => (
       <span className="flex items-center">
         <User size={16} />Name
       </span>
     )
-  }), 
+  }),
   columnsHelper.accessor("email", {
-    cell: (info)=>(
+    cell: (info) => (
       <span className="italic text-blue-600">{info.getValue()}</span>
-    ), 
-    header:()=>(
+    ),
+    header: () => (
       <span className="flex items-center">
-        <User size={16}/>E-mail
+        <User size={16} />E-mail
       </span>
     )
-  }), 
+  }),
   columnsHelper.accessor("phone", {
-    cell: (info)=>(
+    cell: (info) => (
       <span className="">{info.getValue()}</span>
-    ), 
-    header:()=>(
+    ),
+    header: () => (
       <span className="flex items-center">
-        <User size={16}/>Phone
+        <User size={16} />Phone
       </span>
     )
   }),
 ]
 export default function Home() {
-  const [data] = React.useState(()=>[...mockData])
-  const[sorting, setSorting] = React.useState([]); 
+  const [data] = React.useState(() => [...mockData])
+  const [sorting, setSorting] = React.useState([]);
+  const [globalFilter, setGlobalFilter] = React.useState("");
+
   const table = useReactTable({
-    data, 
-    columns, 
-    state:{sorting},
+    data,
+    columns,
+    state: {
+      sorting,
+      globalFilter
+    },
+    initialState: {
+      pagination: {
+        pageSize: 5,
+      },
+    },
     getCoreRowModel: getCoreRowModel(),
-    onSortingChange: setSorting, 
+    onSortingChange: setSorting,
     getSortedRowModel: getSortedRowModel(),
+    onGlobalFilterChange: setGlobalFilter,
+    getFilteredRowModel: getFilteredRowModel(),
   })
   console.log(table.getRowModel())
   return (
-   <div className="flex flex-col min-h-screen max-w-4xl mx-auto py-12 px-4">
+    <div className="flex flex-col min-h-screen max-w-4xl mx-auto py-12 px-4">
+      <div className="mb-4 relative">
+        <input
+          value={globalFilter ?? ""}
+          onChange={(e) => setGlobalFilter(e.target.value)}
+          placeholder="Search..."
+          className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
+        />
+        <Search
+          className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400"
+          size={20}
+        />
+      </div>
       <div className="overflow-x-auto bg-white shadow-md rounded-lg">
         <table className="min-w-full divide-y divide-gray-200 ">
           <thead className="bg-gray-50">
-            {table.getHeaderGroups().map((headerGroup)=>(
+            {table.getHeaderGroups().map((headerGroup) => (
               <tr key={headerGroup.id}>
-                {headerGroup.headers.map((header)=>(
+                {headerGroup.headers.map((header) => (
                   <th key={header.id}>
                     <div {...{
-                        className: header.column.getCanSort()
-                          ? "cursor-pointer select-none flex items-center"
-                          : "",
-                        onClick: header.column.getToggleSortingHandler(),
-                      }}>
+                      className: header.column.getCanSort()
+                        ? "cursor-pointer select-none flex items-center"
+                        : "",
+                      onClick: header.column.getToggleSortingHandler(),
+                    }}>
                       {
                         flexRender(
-                          header.column.columnDef.header, 
+                          header.column.columnDef.header,
                           header.getContext()
                         )
                       }
@@ -95,10 +126,10 @@ export default function Home() {
             ))}
           </thead>
           <tbody className="bg-white divide-y divide-gray-200">
-            {table.getRowModel().rows.map((row)=>(
+            {table.getRowModel().rows.map((row) => (
               <tr key={row.id}>
                 {
-                  row.getVisibleCells().map((cell)=>(
+                  row.getVisibleCells().map((cell) => (
                     <td key={cell.id}>
                       {flexRender(cell.column.columnDef.cell, cell.getContext())}
                     </td>
@@ -109,6 +140,73 @@ export default function Home() {
           </tbody>
         </table>
       </div>
-   </div>
+      <div className="flex flex-col sm:flex-row justify-between items-center mt-4 text-sm text-gray-700">
+        <div className="flex items-center mb-4 sm:mb-0">
+          <span className="mr-2">Items per page</span>
+          <select
+            className="border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 p-2"
+            value={table.getState().pagination.pageSize}
+            onChange={(e) => {
+              table.setPageSize(Number(e.target.value));
+            }}
+          >
+            {[5, 10, 20, 30].map((pageSize) => (
+              <option key={pageSize} value={pageSize}>
+                {pageSize}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div className="flex items-center space-x-2">
+          <button
+            className="p-2 rounded-md bg-gray-100 text-gray-600 hover:bg-gray-200 disabled:opacity-50"
+            onClick={() => table.setPageIndex(0)}
+            disabled={!table.getCanPreviousPage()}
+          >
+            <ChevronsLeft size={20} />
+          </button>
+
+          <button
+            className="p-2 rounded-md bg-gray-100 text-gray-600 hover:bg-gray-200 disabled:opacity-50"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            <ChevronLeft size={20} />
+          </button>
+
+          <span className="flex items-center">
+            <input
+              min={1}
+              max={table.getPageCount()}
+              type="number"
+              value={table.getState().pagination.pageIndex + 1}
+              onChange={(e) => {
+                const page = e.target.value ? Number(e.target.value) - 1 : 0;
+                table.setPageIndex(page);
+              }}
+              className="w-16 p-2 rounded-md border border-gray-300 text-center"
+            />
+            <span className="ml-1">of {table.getPageCount()}</span>
+          </span>
+
+          <button
+            className="p-2 rounded-md bg-gray-100 text-gray-600 hover:bg-gray-200 disabled:opacity-50"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            <ChevronRight size={20} />
+          </button>
+
+          <button
+            className="p-2 rounded-md bg-gray-100 text-gray-600 hover:bg-gray-200 disabled:opacity-50"
+            onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+            disabled={!table.getCanNextPage()}
+          >
+            <ChevronsRight size={20} />
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
